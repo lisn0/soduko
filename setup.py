c3b7a33ca9198d3ab3505bf2a0e39f870aff8036
lisn0 <lisnoxvel@gmail.com>
2021-12-12 18:42:40 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='soduko',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='yorika',
+    author_email='lisnoxvel@gmail.com',
+    description='sudoku expert system solver'
+)
